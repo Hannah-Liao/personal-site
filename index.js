3638ee9229efa35fe919624178dea9df29798e19
Hannah-Liao <hanna.liao@outlook.com>
2023-05-01 21:04:52 +0100

    refactor:refactor code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// stiky navbar & hamburger menu
 window.addEventListener("scroll", () => {
     document.querySelector("header").classList.toggle("sticky", window.scrollY > 100);
     document.querySelector("#menu-icon").classList.remove("bx-x");
     document.querySelector(".nav-list").classList.remove("open");
+
+    document.querySelector("#scroll_up").classList.toggle("show_scroll_up", window.scrollY > 650);
 });
 
 document.querySelector("#menu-icon").onclick = () => {
@@ -9,18 +12,21 @@ document.querySelector("#menu-icon").onclick = () => {
     document.querySelector(".nav-list").classList.toggle("open");
 }
 
-let lightMode = document.querySelector("#lightmode-icon");
+// dark mode
+let darkMode = document.querySelector("#darkmode-icon");
+darkMode.onclick = () => {
+    if (darkMode.classList.contains("bx-moon")) {
+        darkMode.classList.replace("bx-moon", "bx-sun");
+        document.body.classList.add("dark");
+        document.body.classList.add("toggle");
+        document.querySelector("#download_btn").classList.replace("btn-outline-dark", "btn-outline-light")
+        document.querySelector("#contact_info_btn").classList.replace("btn-dark", "btn-light")
 
-lightMode.onclick = () => {
-    if (lightMode.classList.contains("bx-sun")) {
-        lightMode.classList.replace("bx-sun", "bx-moon");
-        document.body.classList.add("light");
-        document.body.classList.add("toggle");
     } else {
-        lightMode.classList.replace("bx-moon", "bx-sun");
-        document.body.classList.remove("light");
+        darkMode.classList.replace("bx-sun", "bx-moon");
+        document.body.classList.remove("dark");
         document.body.classList.add("toggle");
+        document.querySelector("#download_btn").classList.replace("btn-outline-light", "btn-outline-dark")
+        document.querySelector("#contact_info_btn").classList.replace("btn-light", "btn-dark")
     }
 }
-
-
